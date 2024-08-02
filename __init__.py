3c09ef8a13946b9372e44cf36f769c7f7af80d14
Chris <fz8377@partner.kit.edu>
2024-08-02 14:10:08 +0200

    updated imports in init to relative imports

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
-from sdm.models.sdm_reference_model.product import Product
-from sdm.models.sdm_reference_model.resources import Resource
-from sdm.models.sdm_reference_model.procedure import Procedure
-from sdm.models.sdm_reference_model.processes import Process
-from sdm.models.sdm_reference_model.order import Order
-from sdm.models.sdm_reference_model.performance import Performance
-from sdm.models.sdm_reference_model.change_scenario import ChangeScenario
+from .product import Product
+from .resources import Resource
+from .procedure import Procedure
+from .processes import Process
+from .order import Order
+from .performance import Performance
+from .change_scenario import ChangeScenario
 
-from sdm.models.sdm_reference_model.reference_model import ReferenceModel
+from .reference_model import ReferenceModel
