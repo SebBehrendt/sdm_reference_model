d04d41dde80bacfcfba76ed62ab4de7e2462cf2c
Sebastian Behrendt <sebastianbehrendt97@gmail.com>
2024-12-23 20:07:37 +0100

    updated version tag and added version info

--- sdm_reference_model/__init__.py
+++ sdm_reference_model/__init__.py
@@ -1,3 +1,5 @@
+import importlib_metadata
+import toml
 from .product import Product
 from .resources import Resource
 from .procedure import Procedure
@@ -7,3 +9,21 @@ from .performance import Performance
 from .change_scenario import ChangeScenario
 
 from .reference_model import ReferenceModel
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version("sdm_reference_model")
+    except:
+        pass
+    try:
+        pyproject = toml.load("pyproject.toml")
+        return pyproject["tool"]["poetry"]["version"]
+    except:
+        pass
+    raise ModuleNotFoundError(
+        "Could not find version in package metadata or pyproject.toml"
+    )
+
+
+VERSION = get_version()
